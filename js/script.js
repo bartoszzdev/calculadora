6b8438612a1645966d3cf035245bd6dfdaf05b18
bartoszzdev <Vclaudineicampos95@gmail.com>
2021-12-23 17:45:31 -0300

    Melhorando o código

--- js/script.js
+++ js/script.js
@@ -1,73 +1,46 @@
-var botao = document.getElementById("calcular")
-var operacaoSoma = document.querySelector("#somar")
-var operacaoSubt = document.querySelector("#subtrair")
-var operacaoMult = document.querySelector("#multiplicar")
-var operacaoDivd = document.querySelector("#dividir")
+const botao = document.getElementById("calcular")
+const operador = document.querySelectorAll(".operador")
 
-operacaoSoma.onclick = function() {
-    this.classList.add("escolhido")
-    operacaoSubt.classList.remove("escolhido")
-    operacaoMult.classList.remove("escolhido")
-    operacaoDivd.classList.remove("escolhido")
+operador.forEach(btn => {
+    btn.addEventListener("click", e => {
+        e.target.classList.add("escolhido")
+
+        const notChoosen = document.querySelectorAll(".operador:not(" + "." + e.target.classList[1] + ")")
+        notChoosen.forEach(item => {
+            item.classList.remove("escolhido")
+        })
+    })
+})
+
+function operacaoFinal(num1, num2) {
+    let operacao
+
+    if (operador[0].classList.contains("escolhido")) {
+        operacao = num1 + " + " + num2 + " = " + (num1 + num2).toLocaleString()
+    } else if (operador[1].classList.contains("escolhido")) {
+        operacao = num1 + " - " + num2 + " = " + (num1 - num2).toLocaleString()
+    } else if (operador[2].classList.contains("escolhido")) {
+        operacao = num1 + " × " + num2 + " = " + (num1 * num2).toLocaleString()
+    } else if (operador[3].classList.contains("escolhido")) {
+        operacao = num1 + " ÷ " + num2 + " = " + (num1 / num2).toLocaleString()
+    }
+
+    return operacao
 }
 
-operacaoSubt.onclick = function() {
-    this.classList.add("escolhido")
-    operacaoSoma.classList.remove("escolhido")
-    operacaoMult.classList.remove("escolhido")
-    operacaoDivd.classList.remove("escolhido")
-}
+botao.addEventListener("click", () => {
+    const termo1 = document.getElementById("first-num").value
+    const termo2 = document.getElementById("second-num").value
+    const number1 = parseFloat(termo1)
+    const number2 = parseFloat(termo2)
+    const resultado = document.getElementById("result")
 
-operacaoMult.onclick = function() {
-    this.classList.add("escolhido")
-    operacaoSoma.classList.remove("escolhido")
-    operacaoSubt.classList.remove("escolhido")
-    operacaoDivd.classList.remove("escolhido")
-}
+    if(termo1 === "" || termo2 === "") {
+        resultado.innerHTML = "Insira números válidos!"
+    } else {
+        resultado.innerHTML = operacaoFinal(number1, number2)
+    }
 
-operacaoDivd.onclick = function() {
-    this.classList.add("escolhido")
-    operacaoSoma.classList.remove("escolhido")
-    operacaoSubt.classList.remove("escolhido")
-    operacaoMult.classList.remove("escolhido")
-}
-
-botao.onclick = function() {
-        var termo1 = parseFloat(document.getElementById("first-num").value)
-        var termo2 = parseFloat(document.getElementById("second-num").value)
-        var resultado = document.getElementById("result")
-
-        if(document.getElementById("first-num").value === "" || 
-        document.getElementById("second-num").value === "") {
-
-            resultado.innerHTML = "Insira um número válido!"
-
-        } else if(operacaoSoma.classList.contains("escolhido")) {
-
-            var resultadoFinal = termo1 + termo2
-            resultado.innerHTML = termo1.toLocaleString() + " + " + termo2.toLocaleString() + " = " + resultadoFinal.toLocaleString()
-            operacaoSoma.classList.remove("escolhido")
-
-        } else if(operacaoSubt.classList.contains("escolhido")) {
-
-            var resultadoFinal = termo1 - termo2
-            resultado.innerHTML = termo1.toLocaleString() + " - " + termo2.toLocaleString() + " = " + resultadoFinal.toLocaleString()
-            operacaoSubt.classList.remove("escolhido")
-
-        } else if(operacaoMult.classList.contains("escolhido")) {
-
-            var resultadoFinal = termo1 * termo2
-            resultado.innerHTML = termo1.toLocaleString() + " × " + termo2.toLocaleString() + " = " + resultadoFinal.toLocaleString()
-            operacaoMult.classList.remove("escolhido")
-
-        } else if(operacaoDivd.classList.contains("escolhido")) {
-            
-            var resultadoFinal = termo1 / termo2
-            resultado.innerHTML = termo1.toLocaleString() + " ÷ " + termo2.toLocaleString() + " = " + resultadoFinal.toLocaleString()
-            operacaoDivd.classList.remove("escolhido")
-        }
-
-        document.getElementById("first-num").value = ""
-        document.getElementById("second-num").value = ""
-}
-
+    document.getElementById("first-num").value = ""
+    document.getElementById("second-num").value = ""
+})
